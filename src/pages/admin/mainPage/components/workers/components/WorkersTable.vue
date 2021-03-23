a246162309d8619040577f86fb367934e7282c06
janar <janar.kallson@gmail.com>
2021-03-23 23:03:35 +0200

    Fixed deletion text

--- src/pages/admin/mainPage/components/workers/components/WorkersTable.vue
+++ src/pages/admin/mainPage/components/workers/components/WorkersTable.vue
@@ -63,15 +63,14 @@
         <v-dialog v-model="dialogDelete" max-width="500px">
           <v-card>
             <v-card-title class="headline"
-              >Are you sure you want to delete this item?</v-card-title
+              >Kas soovite tõesti kustutada töötaja
+              {{ editedItem.name }} ?</v-card-title
             >
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete"
-                >Cancel</v-btn
-              >
+              <v-btn color="blue darken-1" text @click="closeDelete">Ei</v-btn>
               <v-btn color="blue darken-1" text @click="deleteItemConfirm"
-                >OK</v-btn
+                >Jah</v-btn
               >
               <v-spacer></v-spacer>
             </v-card-actions>
