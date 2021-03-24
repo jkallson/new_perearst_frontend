ae321cd588475ddadf3098fefea08d3e1604c06b
janar <janar.kallson@gmail.com>
2021-03-24 13:54:30 +0200

    Fixed admin homepage margins

--- src/pages/admin/mainPage/views/AdminHomePage.vue
+++ src/pages/admin/mainPage/views/AdminHomePage.vue
@@ -5,8 +5,8 @@
         <AdminAboutUs></AdminAboutUs>
       </v-col>
     </v-row>
-    <v-row no-gutters class="pt-5">
-      <v-col>
+    <v-row class="pt-5 mt-0">
+      <v-col class="pt-0">
         <AdminWorkers></AdminWorkers>
       </v-col>
     </v-row>
@@ -17,34 +17,31 @@
     </v-row>
     <v-row no-gutters class="pt-5">
       <v-col>
-        <AdminContactInformation></AdminContactInformation>
+        <AdminUsefulLinks></AdminUsefulLinks>
       </v-col>
     </v-row>
     <v-row no-gutters class="pt-5">
       <v-col>
-        <v-card>
-          <BasicToolbar name="Kasulikud lingid"></BasicToolbar>
-          <p>asd</p>
-        </v-card>
+        <AdminContactInformation></AdminContactInformation>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import BasicToolbar from "@/components/BasicToolbar";
 import AdminAboutUs from "@/pages/admin/mainPage/components/aboutUs/AdminAboutUs";
 import AdminWorkers from "@/pages/admin/mainPage/components/workers/AdminWorkers";
 import AdminPrices from "@/pages/admin/mainPage/components/prices/AdminPrices";
 import AdminContactInformation from "@/pages/admin/mainPage/components/contactInformation/AdminContactInformation";
+import AdminUsefulLinks from "@/pages/admin/mainPage/components/usefulLinks/AdminUsefulLinks";
 export default {
   name: "AdminHomePage",
   components: {
+    AdminUsefulLinks,
     AdminContactInformation,
     AdminPrices,
     AdminWorkers,
-    AdminAboutUs,
-    BasicToolbar
+    AdminAboutUs
   }
 };
 </script>
