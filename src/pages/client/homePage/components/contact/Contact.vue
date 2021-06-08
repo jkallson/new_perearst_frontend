3d0d503e0fb9b99bc56144782d7e8eb22ac68cc4
janar <janar.kallson@gmail.com>
2021-06-08 10:31:32 +0300

    Removed email sending until I can get it fixed

--- src/pages/client/homePage/components/contact/Contact.vue
+++ src/pages/client/homePage/components/contact/Contact.vue
@@ -1,15 +1,14 @@
 <template>
   <v-container fluid class="text-center pb-10">
     <SectionName name="VÕTKE ÜHENDUST"></SectionName>
-    <Map></Map>
-    <v-row class="px-5">
+    <v-row class="px-5 align-center">
       <v-col cols="12" lg="6" md="6" sm="12" xs="12">
         <ContactInformation
           :contact-information="contactInformation"
         ></ContactInformation>
       </v-col>
       <v-col cols="12" lg="6" md="6" sm="12" xs="12">
-        <EmailForm></EmailForm>
+        <Map></Map>
       </v-col>
     </v-row>
   </v-container>
@@ -18,11 +17,10 @@
 <script>
 import Map from "@/pages/client/homePage/components/contact/components/Map";
 import ContactInformation from "@/pages/client/homePage/components/contact/components/ContactInformation";
-import EmailForm from "@/pages/client/homePage/components/contact/components/EmailForm";
 import SectionName from "@/components/SectionName";
 export default {
   name: "Contact",
-  components: { SectionName, EmailForm, ContactInformation, Map },
+  components: { SectionName, ContactInformation, Map },
   props: {
     contactInformation: {
       type: Object,
